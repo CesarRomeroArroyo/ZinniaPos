9f6fb21e6d50a8535a5d6c7f1cece80e2c6923ed
Jorge Mendez <jorge.mendezj@cecar.edu.co>
2024-11-13 10:36:44 -0500

    Modulo de login

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { LoginPage } from './login.page';
+import { IconInputDirective } from 'src/app/core/directives/icon-input.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    ReactiveFormsModule,
+    LoginPageRoutingModule,
+    IconInputDirective,
   ],
   declarations: [LoginPage]
 })
--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,29 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../../core/services/local-storage.service';
 import { ProxyService } from '../../core/services/proxy.service';
-import { ToastController } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { IonButton, IonCheckbox, IonContent, IonFooter, IonInput, IonToolbar, ModalController, ToastController } from '@ionic/angular';
+import { Router, RouterModule } from '@angular/router';
 import { TranslationService } from '../../core/services/translation.service';
+import { LoginImages } from './login.consts';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { mailOutline, lockClosedOutline, eyeOutline, eyeOffOutline } from 'ionicons/icons';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
   public usuario:any =  {user: '', pass:''};
+  public loginImages = LoginImages
+  public loginForm!: FormGroup;
+
   constructor(
     public translationService: TranslationService,
     private proxy:ProxyService,
     private localStorage: LocalStorageService,
     private toastController: ToastController,
-    private router: Router
-  ) { }
+    private router: Router,
+    private _formBuild: FormBuilder,
+  ) { 
+    addIcons({ mailOutline, lockClosedOutline, eyeOutline, eyeOffOutline });
+  }
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.createForm();
+  }
+
+  private createForm(): void {
+    this.loginForm = this._formBuild.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    //this.validateRememberSession();
   }
 
   async login(){
-    const user: any = await this.proxy.postMethod('login/', this.usuario);
+      const user: any = await this.proxy.postMethod('login/', this.usuario);
     console.log(user);
     if(user.length > 0){
       this.localStorage.setItem('ZINNIA_USER', user[0]);
