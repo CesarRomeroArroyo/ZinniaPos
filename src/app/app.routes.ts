1d117063618d9bc55b9261833034596fdab0d95f
Jorge Mendez <jorge.mendezj@cecar.edu.co>
2024-11-18 08:43:38 -0500

    Se añade ruta para modulo de citas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,10 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
     canActivate: [UnauthenticatedGuard],
+  },
+  {
+    path: 'appointments',
+    loadComponent: () => import('./pages/dashboard/appointments/appointments.component').then((m) => m.AppointmentsComponent),
+    canActivate: [UnauthenticatedGuard],
   }
 ];
